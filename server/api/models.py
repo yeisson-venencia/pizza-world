c28c27b4b3e3d3458d5ba2a6958dec9d9bd45692
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-15 23:43:27 -0400

    s11/class-one: added class Customer to the model

--- server/api/models.py
+++ server/api/models.py
@@ -17,7 +17,16 @@ class Profile(models.Model):
     birth_date = models.DateField(auto_now=False, auto_now_add=False)
     creation_date = models.DateTimeField(auto_now=False, auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True, auto_now_add=True)
+    customer = models.OneToOneField(Customer,on_delete=models.CASCADE,related_name='profile_of')
 
     class Meta:
-        db_table = 'profile' 
-  
+        db_table = 'profile'
+
+class Customer(models.Model):
+    email = models.CharField(max_lenght=100)
+    password = models.CharField(max_lenght=100)
+    creation_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True, auto_now_add=True)
+
+    class Meta:
+        db_table = 'customer'    
