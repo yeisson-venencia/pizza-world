d9968123c7974c00797f09e15b8602a9d5759167
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-16 15:01:38 -0400

    p02/home: adding lonks to the nav bar

--- customer_client/src/components/common/TheNav.vue
+++ customer_client/src/components/common/TheNav.vue
@@ -1,5 +1,34 @@
 <template>
-  <v-app-bar app color="primary" dark>
-    <v-img></v-img>
+  <v-app-bar app color="red" dark>
+    <v-img src="../../assets/pizza_world_logo_text.png" max-width="160"></v-img>
+    <v-spacer></v-spacer>
+    <v-row>
+      <v-btn
+        v-for="section in views"
+        :key="section.label"
+        plain
+        :to="section.link"
+        color="black"
+        ><h2>{{ section.label }}</h2></v-btn
+      >
+    </v-row>
+    <v-spacer> </v-spacer>
+    <v-btn>Log In</v-btn>
   </v-app-bar>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      views: [
+        { link: "/", label: "Home" },
+        { link: "/product", label: "Product" },
+        { link: "/order", label: "Order" },
+        { link: "/about", label: "About" },
+        { link: "/contanct", label: "Contact us" },
+      ],
+    };
+  },
+};
+</script>
