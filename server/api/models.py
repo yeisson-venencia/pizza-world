f1809321147ea3288ddfee09ba55f8ae8de39f63
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-15 23:37:51 -0400

    s11/class-one: added class Profile to the model

--- server/api/models.py
+++ server/api/models.py
@@ -4,6 +4,20 @@ from django.db import models
 
 class Role(models.Model):
     name = models.CharField(max_lenght=50)
+    creation_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True, auto_now_add=True)
 
     class Meta:
         db_table = 'role'
+
+class Profile(models.Model):
+    first_name = models.CharField(max_lenght=100)
+    last_name = models.CharField(max_lenght=100)
+    document = models.CharField(max_lenght=50)
+    birth_date = models.DateField(auto_now=False, auto_now_add=False)
+    creation_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True, auto_now_add=True)
+
+    class Meta:
+        db_table = 'profile' 
+  
