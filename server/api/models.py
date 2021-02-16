9182bfaf6222935d850519e3dfc30a13da3be113
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-15 23:51:17 -0400

    s11/class-one: added class Receptor to the model

--- server/api/models.py
+++ server/api/models.py
@@ -10,18 +10,6 @@ class Role(models.Model):
     class Meta:
         db_table = 'role'
 
-class Profile(models.Model):
-    first_name = models.CharField(max_lenght=100)
-    last_name = models.CharField(max_lenght=100)
-    document = models.CharField(max_lenght=50)
-    birth_date = models.DateField(auto_now=False, auto_now_add=False)
-    creation_date = models.DateTimeField(auto_now=False, auto_now_add=True)
-    update_date = models.DateTimeField(auto_now=True, auto_now_add=True)
-    customer = models.OneToOneField(Customer,on_delete=models.CASCADE,related_name='profile_of')
-
-    class Meta:
-        db_table = 'profile'
-
 class Customer(models.Model):
     email = models.CharField(max_lenght=100)
     password = models.CharField(max_lenght=100)
@@ -29,4 +17,29 @@ class Customer(models.Model):
     update_date = models.DateTimeField(auto_now=True, auto_now_add=True)
 
     class Meta:
-        db_table = 'customer'    
+        db_table = 'customer'
+
+class Profile(models.Model):
+    first_name = models.CharField(max_lenght=100)
+    last_name = models.CharField(max_lenght=100)
+    document = models.CharField(max_lenght=50)
+    phone = models.CharField(max_lenght=50)
+    birth_date = models.DateField(auto_now=False, auto_now_add=False)
+    creation_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True, auto_now_add=True)
+    customer = models.OneToOneField(Customer,on_delete=models.CASCADE,related_name='profile_of')
+
+    class Meta:
+        db_table = 'profile'
+class Receptor(models.Model):
+    first_name = models.CharField(max_lenght=100)
+    last_name = models.CharField(max_lenght=100)
+    document = models.CharField(max_lenght=50)
+    phone = models.CharField(max_lenght=50)
+    address = models.CharField(max_lenght=250)
+    creation_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True, auto_now_add=True)
+    customer = models.ForeignKey(Customer,related_name='receptor_of', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'receptor'   
