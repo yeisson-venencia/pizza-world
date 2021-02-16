beef1968fcc650d47db5827acfbe30a42958b985
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-15 22:13:22 -0400

    p01/init: adding configuration to use postgres data base in setting.py

--- server/server/settings.py
+++ server/server/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+# Initialise enviroment variables
+
+env = environ.Env()
+
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'eke+dk*&x2=o@)!%_x!rtk1w#pe!&ei@c5a4+y*zl2ua@s9%tj'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,11 +81,31 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+} """
+
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': env('DATABASE_NAME'),
+
+        'USER': env('DATABASE_USER'),
+
+        'PASSWORD': env('DATABASE_PASSWORD'),
+
+        'HOST': env('DATABASE_HOST'),
+
+        'PORT': env('DATABASE_PORT'),
+
+    }
+
 }
 
 
