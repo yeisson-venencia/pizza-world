8bb999bef9a344a33d99d0a850e5f04abbc4687b
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-16 21:39:33 -0400

    p02/home: finishing presentation banner

--- customer_client/src/views/Home.vue
+++ customer_client/src/views/Home.vue
@@ -1,5 +1,14 @@
 <template>
-  <v-row class="home-container"> </v-row>
+  <v-row class="home-container">
+    <v-col class="home-col" align="center">
+      <v-row class="home-message-container" align="center">
+        <v-col class="home-slogan">
+          <h1>THE ONLY PLACE IN THE WORLD</h1>
+          <h2>"... with the best taste in the Pizza!!!"</h2>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -11,6 +20,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../styles/main.scss";
 .home-container {
   background-image: url("../assets/home_main.jpg");
   background-position: bottom;
@@ -19,5 +29,20 @@ export default {
   height: 100%;
   font-size: 22px;
   width: 100%;
+}
+
+.home-col {
+  background: rgb(20, 24, 19, 0.6);
+}
+
+.home-message-container {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+.home-slogan {
+  color: rgb(236, 234, 234);
+  font-size: 150%;
 }
 </style>
