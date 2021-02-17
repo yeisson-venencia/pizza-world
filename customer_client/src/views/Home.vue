df74850310bcdc73137d07f4dba82726db9f3e7c
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-16 22:05:59 -0400

    p02/home: adding product section to home

--- customer_client/src/views/Home.vue
+++ customer_client/src/views/Home.vue
@@ -1,14 +1,21 @@
 <template>
-  <welcome-section></welcome-section>
+  <div>
+    <welcome-section></welcome-section>
+    <product-section></product-section>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import WelcomeSection from "./../components/home/WelcomeSection";
+import ProductSection from "./../components/home/ProductSection";
 
 export default {
   name: "Home",
-  components: { "welcome-section": WelcomeSection },
+  components: {
+    "welcome-section": WelcomeSection,
+    "product-section": ProductSection,
+  },
 };
 </script>
 
