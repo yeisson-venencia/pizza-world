96c9752075717693a5c067e3097cc8394fd21c4d
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-17 00:05:41 -0400

    p02/home: adding products to the home page with images

--- customer_client/src/components/home/ProductSection.vue
+++ customer_client/src/components/home/ProductSection.vue
@@ -1,3 +1,47 @@
 <template>
-  <v-row class="home-container"> </v-row>
+  <v-row class="home-container" align="center" justify="space-around">
+    <v-card max-width="374" v-for="product in products" :key="product.title">
+      <v-img height="300" :src="resolve_img_url(product.imagePath)"></v-img>
+      <v-card-title>{{ product.title }}</v-card-title>
+      <v-card-subtitle> {{ product.subtitle }}</v-card-subtitle>
+      <v-card-text>{{ product.text }}</v-card-text>
+    </v-card>
+  </v-row>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        {
+          imagePath: "pizza.jpg",
+          title: "Pizzas",
+          subtitle: "The best pizzas in the area",
+          text: "Cooked with the most aromatic ingredients",
+        },
+        {
+          imagePath: "milkshake.jpg",
+          title: "Milkshakes",
+          subtitle: "The best pizzas in the area",
+          text: "Cooked with the most aromatic ingredients",
+        },
+        {
+          imagePath: "dessert.jpg",
+          title: "Desserts",
+          subtitle: "The best pizzas in the area",
+          text: "Cooked with the most aromatic ingredients",
+        },
+      ],
+    };
+  },
+  methods: {
+    resolve_img_url(path) {
+      const images = require.context("../../assets", false, /\.png$|\.jpg$/);
+      return images("./" + path);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
