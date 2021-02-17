7f8822df1f8ad22095521acfbd5d4ae7456e4927
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-16 22:02:42 -0400

    p02/home: creating component for welcome section

--- customer_client/src/views/Home.vue
+++ customer_client/src/views/Home.vue
@@ -1,48 +1,15 @@
 <template>
-  <v-row class="home-container">
-    <v-col class="home-col" align="center">
-      <v-row class="home-message-container" align="center">
-        <v-col class="home-slogan">
-          <h1>THE ONLY PLACE IN THE WORLD</h1>
-          <h2>"... with the best taste in the Pizza!!!"</h2>
-        </v-col>
-      </v-row>
-    </v-col>
-  </v-row>
+  <welcome-section></welcome-section>
 </template>
 
 <script>
 // @ is an alias to /src
+import WelcomeSection from "./../components/home/WelcomeSection";
 
 export default {
   name: "Home",
+  components: { "welcome-section": WelcomeSection },
 };
 </script>
 
-<style lang="scss" scoped>
-@import "../styles/main.scss";
-.home-container {
-  background-image: url("../assets/home_main.jpg");
-  background-position: bottom;
-  margin: 0;
-  background-size: cover;
-  height: 100%;
-  font-size: 22px;
-  width: 100%;
-}
-
-.home-col {
-  background: rgb(20, 24, 19, 0.6);
-}
-
-.home-message-container {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-}
-
-.home-slogan {
-  color: rgb(236, 234, 234);
-  font-size: 150%;
-}
-</style>
+<style lang="scss" scoped></style>
