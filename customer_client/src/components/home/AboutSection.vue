ae349c973cba69aab5d958b2f7cce8f030fe36fe
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-17 13:03:39 -0400

    p02/home: finishing layout for about section

--- customer_client/src/components/home/AboutSection.vue
+++ customer_client/src/components/home/AboutSection.vue
@@ -1,24 +1,26 @@
 <template>
-  <v-row
-    class="home-container about-container"
-    alingn="center"
-    justify="center"
-  >
-    <v-col class="column-container" align="center">
-      <v-row>
+  <v-row class="home-container about-container" align="center" justify="center">
+    <v-col class="column-container">
+      <v-row class="about-text-container" align="end">
         <v-col align="center">
           <h2>Our company</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
-            magni!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quos
+            modi id dolorum eius dolores nobis laborum omnis molestiae dolore.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quos
+            modi id dolorum eius dolores nobis laborum omnis molestiae dolore.
           </p>
         </v-col>
       </v-row>
 
-      <v-row aling="center" justify="space-around">
-        <div class="test"></div>
-        <div class="test"></div>
-        <div class="test"></div>
+      <v-row
+        class="about-images-container"
+        align="center"
+        justify="space-around"
+      >
+        <div class="about-image about-image-message"></div>
+        <div class="about-image about-image-logo"></div>
+        <div class="about-image"></div>
       </v-row>
     </v-col>
   </v-row>
@@ -32,12 +34,22 @@
 
 .column-container {
   max-width: 80%;
+  height: 90%;
 }
 
-.test {
+.about-text-container {
+  height: 40%;
+}
+
+.about-images-container {
+  background-color: white;
+  height: 60%;
+}
+
+.about-image {
   background-color: $success-color;
-  width: 300px;
-  height: 300px;
-  border-radius: 20px;
+  width: 25%;
+  height: 90%;
+  border-radius: 12px;
 }
 </style>
